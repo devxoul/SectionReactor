9b8a91cf9742aad666e540974dcf17d84540572e
Suyeol Jeon <devxoul@gmail.com>
2017-09-09 21:38:13 -0400

    Make empty views/cells hidden

--- Sources/SectionReactor/Empty.swift
+++ Sources/SectionReactor/Empty.swift
@@ -5,13 +5,17 @@ public extension UICollectionView {
   func emptyCell(for indexPath: IndexPath) -> UICollectionViewCell {
     let identifier = "SectionReactor.UICollectionView.emptyCell"
     self.register(UICollectionViewCell.self, forCellWithReuseIdentifier: identifier)
-    return self.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+    let cell = self.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+    cell.isHidden = true
+    return cell
   }
 
   func emptyView(for indexPath: IndexPath, kind: String) -> UICollectionReusableView {
     let identifier = "SectionReactor.UICollectionView.emptyView"
     self.register(UICollectionReusableView.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: identifier)
-    return self.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: identifier, for: indexPath)
+    let view = self.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: identifier, for: indexPath)
+    view.isHidden = true
+    return view
   }
 }
 
@@ -19,7 +23,9 @@ public extension UITableView {
   func emptyCell(for indexPath: IndexPath) -> UITableViewCell {
     let identifier = "SectionReactor.UITableView.emptyCell"
     self.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
-    return self.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    cell.isHidden = true
+    return cell
   }
 }
 #endif
