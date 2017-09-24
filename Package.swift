a93ad583582fdbbefaa7609b349c47b7364830ee
Suyeol Jeon <devxoul@gmail.com>
2017-09-24 19:00:22 +0900

    Update Package.swift to V4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
-import Foundation
 import PackageDescription
-
-var dependencies: [Package.Dependency] = [
-  .Package(url: "https://github.com/ReactorKit/ReactorKit.git", majorVersion: 0),
-  .Package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", majorVersion: 2),
-]
-
-let isTest = ProcessInfo.processInfo.environment["TEST"] == "1"
-if isTest {
-  dependencies.append(
-    .Package(url: "https://github.com/devxoul/RxExpect.git", majorVersion: 0)
-  )
-}
 
 let package = Package(
   name: "SectionReactor",
-  dependencies: dependencies
+  products: [
+    .library(name: "SectionReactor", targets: ["SectionReactor"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactorKit/ReactorKit.git", .branch("swift-4.0")),
+    .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .branch("swift4.0")),
+    .package(url: "https://github.com/devxoul/RxExpect", .branch("swift-4.0")),
+  ],
+  targets: [
+    .target(name: "SectionReactor", dependencies: ["ReactorKit", "RxDataSources"]),
+    .testTarget(name: "SectionReactorTests", dependencies: ["SectionReactor", "RxExpect"])
+  ]
 )
