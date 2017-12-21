ea9b4b4a0ee353e68bac9c1a2b18de5cb05e2508
Suyeol Jeon <devxoul@gmail.com>
2017-12-22 04:39:01 +0900

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "3.0.1")),
-    .package(url: "https://github.com/devxoul/RxExpect", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/devxoul/RxExpect.git", .upToNextMajor(from: "1.0.0")),
   ],
   targets: [
     .target(name: "SectionReactor", dependencies: ["ReactorKit", "RxDataSources"]),
